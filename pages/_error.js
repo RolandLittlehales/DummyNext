1d8a2d0f883cd01917086a4ebfb9d3cb0bf535cf
rolandl <rlittlehales@pfdfoods.com.au>
2019-05-01 16:04:41 +1000

    init commit

---
+++ pages/_error.js
@@ -0,0 +1,10 @@
+import Layout from "../components/MyLayout.js";
+
+export default function Error() {
+  return (
+    <Layout>
+      <h1>Error Page</h1>
+      <ul />
+    </Layout>
+  );
+}
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Layout from "../components/MyLayout.js";
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>Aboutg</h1>
+      <p>This is the about page</p>
+    </Layout>
+  );
+}
